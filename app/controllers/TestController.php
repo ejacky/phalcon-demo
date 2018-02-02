45023ee8cb17699b590eb193c296e125c614792d
张健飞 <zhangjianfei@360.net>
2018-02-02 10:21:42 +0800

    add

---
+++ app/controllers/TestController.php
@@ -0,0 +1,21 @@
+<?php
+
+class TestController extends \Phalcon\Mvc\Controller
+{
+
+    public function indexAction()
+    {
+        echo "this is test";
+
+    }
+
+    public function myAction()
+    {
+        echo "this is test";
+
+    }
+
+
+
+}
+
